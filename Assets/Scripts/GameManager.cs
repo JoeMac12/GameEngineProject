06a295bfefd6b459fe4fdd1b458bc46e9d4d772f
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-03-09 00:16:33 -0400

    awesome auto correct moment

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro.EditorUtilities;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
